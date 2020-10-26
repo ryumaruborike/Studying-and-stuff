6168dffdc1e17391bb377028554958dc50cd609e
Daniil <wenom1977@mail.ru>
2020-10-26 09:28:09 +0300

    yep

--- Main/src/Test.java
+++ Main/src/Test.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        System.out.println (y < 2 - x * x && y > x ? "YES": "NO");
     }
 }
